94ee03d9f66bb1941b8e45bab7ee81832ee73976
MauryTiteux <115343276+MauryTiteux@users.noreply.github.com>
2023-03-12 06:15:59 +0100

    oups

--- FoodSearchTutorial/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.Input;
-global using FoodSearchTutorial.Model;
-global using FoodSearchTutorial.View;
-global using FoodSearchTutorial.ViewModel;
-global using FoodSearchTutorial.Services;
-global using System.Collections.ObjectModel;
