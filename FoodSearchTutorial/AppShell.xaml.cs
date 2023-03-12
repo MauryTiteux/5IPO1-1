132920d7ffb84e2f1949021f7f09449d7ee74af6
MauryTiteux <115343276+MauryTiteux@users.noreply.github.com>
2023-03-12 06:15:30 +0100

    2

---
+++ FoodSearchTutorial/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FoodSearchTutorial
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(DetailsPage), typeof(DetailsPage));
+
+
+        }
+    }
+}
